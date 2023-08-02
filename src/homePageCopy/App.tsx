736eaea157aa036f57d378b1b201a4477457f16a
sunsunmonkey <2933201543@qq.com>
2023-08-02 16:51:17 +0800

    homePageCopy

---
+++ src/homePageCopy/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+  <div className="ball">
+      <div className="circle-line1"></div>
+      <div className="circle-line2"></div>
+      <div className="circle-line3"></div>
+    </div>
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./selectPage/App.tsx";
+import App from "./homePageCopy/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
